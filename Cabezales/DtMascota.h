e719de527e6ab6ea2589ed04500cc9969581b6e9
AnimA97 <37343636+AnimA97@users.noreply.github.com>
2018-03-23 14:57:40 -0300

    Sobrecarga <<
    Idem cambios en los .cpp bajo el titulo "Sobrecarga <<"

--- Cabezales/DtMascota.h
+++ Cabezales/DtMascota.h
@@ -17,10 +17,11 @@ class DtMascota {
         DtMascota(string nombre, Genero genero, float peso, float racionDiaria);
         virtual ~DtMascota();
 
-        string getNombre();
-        Genero getGenero();
-        float getPeso();
-        float getRacionDiaria();
+        string getNombre() const;
+        Genero getGenero() const;
+        float getPeso() const;
+        float getRacionDiaria() const;
+    	friend ostream& operator << (ostream &o,const DtMascota &p);
 };
 
 #endif
